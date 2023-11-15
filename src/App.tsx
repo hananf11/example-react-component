09116bdfcc2a56f2c10cea9dc4a979451d9945db
Hanan Fokkens <hananfokkens@gmail.com>
2023-11-16 10:42:52 +1300

    initial commit
    setup react project with vite
    added tailwind
    added pre-commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <span className="text-5xl">Hello, world</span>
+    </div>
+  )
+}
+
+export default App

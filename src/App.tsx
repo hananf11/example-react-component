4e5dd7150eef9e99019c21f72d82139b606951b3
Hanan Fokkens <hananfokkens@gmail.com>
2023-11-16 12:01:44 +1300

    added user data to post
    moved the profiles and thumbnails into public. This is probably not ideal and they should be moved back later and loaded properly from assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import Post from "./Post"
 
-interface User {
+export interface User {
   name: string,
   profile: string
 }
 
-interface PostAsset {
+export interface PostAsset {
   type: 'video' | 'picture',
   thumbnail: string,
 }
@@ -23,22 +23,22 @@ const posts: Post[] = [
     id: 1,
     user: {
       name: "Andrew D. Huberman",
-      profile: './assets/profiles/e51b17fe66bd1a43cd3641d696b8e57e.png'
+      profile: './profiles/e51b17fe66bd1a43cd3641d696b8e57e.png'
     },
     content: "I've found great assistance in this routine management app dashboard. With its customizable reminder feature, I no longer miss out on important tasks. This app has truly helped me enhance my productivity and work efficiency.",
     rating: 4,
     assets: [
       {
         type: 'video',
-        thumbnail: './assets/thumbnails/4b46109e0894ee518b425a3979b6e8b2.jpg'
+        thumbnail: './thumbnails/4b46109e0894ee518b425a3979b6e8b2.jpg'
       },
       {
         type: 'picture',
-        thumbnail: './assets/thumbnails/88df0d24829cbefdf2796cbcd125ac25.jpg'
+        thumbnail: './thumbnails/88df0d24829cbefdf2796cbcd125ac25.jpg'
       },
       {
         type: 'picture',
-        thumbnail: './assets/thumbnails/f0d82c663bf379be8e64d0242fa8d7f8.jpg'
+        thumbnail: './thumbnails/f0d82c663bf379be8e64d0242fa8d7f8.jpg'
       }
     ]
   },
@@ -46,7 +46,7 @@ const posts: Post[] = [
     id: 2,
     user: {
       name: 'David Goggins',
-      profile: './assets/profiles/b7243911db5434081b515415a5e3b387.png'
+      profile: './profiles/b7243911db5434081b515415a5e3b387.png'
     },
     content: "I've found great assistance in this routine management app dashboard. With its customizable reminder feature, I no longer miss out on important tasks. This app has truly helped me enhance my productivity and work efficiency.",
     rating: 4,
@@ -56,22 +56,22 @@ const posts: Post[] = [
     id: 3,
     user: {
       name: 'Tim Ferris',
-      profile: './assets/profiles/8eae073e9d7196f0bb767c36c1b2065f.png'
+      profile: './profiles/8eae073e9d7196f0bb767c36c1b2065f.png'
     },
     content: "I've found great assistance in this routine management app dashboard. With its customizable reminder feature, I no longer miss out on important tasks. This app has truly helped me enhance my productivity and work efficiency.",
     rating: 4,
     assets: [
       {
         type: 'picture',
-        thumbnail: './assets/thumbnails/3a4d08f7e7237ba0f781d197013295e6.jpg'
+        thumbnail: './thumbnails/3a4d08f7e7237ba0f781d197013295e6.jpg'
       },
       {
         type: 'picture',
-        thumbnail: './assets/thumbnails/1526b9d004cb38d9e5d16a9ff3f67df8.jpg'
+        thumbnail: './thumbnails/1526b9d004cb38d9e5d16a9ff3f67df8.jpg'
       },
       {
         type: 'picture',
-        thumbnail: './assets/thumbnails/ff657dfb0256d7d1a44049c3153ad0d3.jpg'
+        thumbnail: './thumbnails/ff657dfb0256d7d1a44049c3153ad0d3.jpg'
       }
     ]
   }
@@ -83,7 +83,7 @@ function App() {
     <div className="h-screen bg-neutral-800 p-10">
       <h1 className="text-2xl font-semibold text-white mb-5">Testimonials</h1>
       <div className="flex text-white gap-5">
-        {posts.map((post) => <Post key={post.id}></Post>)}
+        {posts.map((post) => <Post key={post.id} user={post.user} />)}
       </div>
     </div>
   )
--- src/Post.tsx
+++ src/Post.tsx
@@ -1,7 +1,18 @@
-export default function PostCard() {
+import type { User } from './App.tsx'
+
+export default function Post({ user }: { user: User}) {
+
     return (
-        <div className="bg-neutral-1200 border-neutral-1000 border rounded-2xl flex p-5 gap-5 w-[323px]">
-            <div></div>
+        <div className="bg-neutral-1200 border-neutral-1000 border rounded-2xl flex flex-col p-5 gap-5 w-[323px]">
+            <div className="flex gap-4 rounded-full font-semibold">
+                <img src={user.profile} className="w-10 h-10" />
+                <div className="flex flex-col gap-2">
+                    <span className="font-semibold text-base">{user.name}</span>
+                    <div>
+                        {/* stars go here */}
+                    </div>
+                </div>
+            </div>
             <div></div>
             <div></div>
         </div>
